4706dd18179413c42d21ff4f448618efefa288f8
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-04-30 17:48:25 +0300

    feat: add DNS stat solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const result = {};
+
+  for(let domain of domains) {
+    const domainElements = domain.split('.').reverse();
+    let domainStr = '';
+
+    for(let elem of domainElements) {
+      domainStr += '.' + elem;
+      if(result[domainStr])
+        result[domainStr]++;
+      else
+        result[domainStr] = 1;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
